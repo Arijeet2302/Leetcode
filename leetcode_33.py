ae4381a7d71e2be5a1fea0c93702b1fb973f6670
Arijeet Sinha <arijeetsinha2@gmail.com>
2023-12-28 12:28:24 +0530

    problem added

--- leetcode_33.py
+++ leetcode_33.py
@@ -1,28 +1,23 @@
-def Serach_RotatedArray(arr,k):
-    max_num=max(arr)
-    index=arr.index(max_num)
-    left=arr[0:index+1]
-    right=arr[index+1:]
-    if left[0]<=k and k<=left[-1]:
-        print(8)
-        arr=left
-        low=0
-        high=len(arr)-1
-        while low<=high:
-                mid=(low+high)//2
-                if arr[mid]<k:low=mid+1
-                elif arr[mid]>k:high=mid-1
-                else:return mid
-        return -1
-    else:
-        arr=right
-        low=0
-        high=len(arr)-1
-        while low<=high:
-                mid=(low+high)//2
-                if arr[mid]<k:low=mid+1
-                elif arr[mid]>k:high=mid-1
-                else:return mid+(index+1)
+def Serach_RotatedArray(nums, target):
+        low, high = 0, len(nums) - 1
+
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid] and nums[low] <= target < nums[mid]:
+                high = mid - 1
+            elif nums[mid] <= nums[high] and nums[mid] < target <= nums[high]:
+                low = mid + 1
+            else:
+                if nums[mid] <= nums[high]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
         return -1
 
 print(Serach_RotatedArray([1,3],1))
